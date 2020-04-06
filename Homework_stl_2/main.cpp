27c6e2fcca73e94d9c8a76378797262113d01d82
Ershov-Artem <63228781+Ershov-Artem@users.noreply.github.com>
2020-04-06 13:27:45 +0300

    Update main.cpp

--- Homework_stl_2/main.cpp
+++ Homework_stl_2/main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <cstring>
 #include <vector>
@@ -9,24 +10,24 @@ struct man{
 public:
 string Familia ;
 string Name;
-int math;
-int phis;
-int it;
+int score;
+
 };
 bool comp(const man &a, const man &b){
-int tmp1 = a.math + a.phis + a.it;
-int tmp2 = b.math + b.phis + b.it;
+int tmp1 = a.score;
+int tmp2 = b.score;
 return tmp1 > tmp2 ? true : false;
 }
 
 int main()
 {
-int n;
+int n,a,b,c;
 cin>> n;
 vector< man > A(n);
 for (int i = 0; i < n; i++) {
 cin >> A[i].Name>> A[i].Familia;
-cin>> A[i].math >> A[i].phis >> A[i].it;
+cin>> a >> b >> c;
+A[i].score = a+b+c;
     }
 
     sort(A.begin(), A.end(), comp);
